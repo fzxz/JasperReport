918751c103e29b25f053fa9e4d1a98b1e23a4455
TaniaMarchuk <t.n.marchuk@gmail.com>
2016-03-29 14:43:12 +0300

    Jasper Reports, iReports, iText, SpringMVC, JPA, H2, JPS, HTML, CSS

--- src/main/java/ua/sevenLevelLabs/config/InfrastructureConfig.java
+++ src/main/java/ua/sevenLevelLabs/config/InfrastructureConfig.java
@@ -32,12 +32,6 @@ public class InfrastructureConfig {
 
     @Bean
     public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName(env.getProperty("db.driver"));
-//        dataSource.setUrl(env.getProperty("db.url"));
-//        dataSource.setUsername(env.getProperty("db.username"));
-//        dataSource.setPassword(env.getProperty("db.password"));
-//        return dataSource;
 
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
         EmbeddedDatabase db = builder
